bd299fe15fd3d7dcf233c6a6cf20cc66747bd5a8
ninad-techprescient <86280948+ninad-techprescient@users.noreply.github.com>
2024-01-23 23:06:11 +0530

    interval added to tui mode

--- kafka_lag_monitor/tui.py
+++ kafka_lag_monitor/tui.py
@@ -4,25 +4,10 @@ from textual.widgets import Header, Footer, DataTable, ProgressBar
 from textual.containers import ScrollableContainer
 from kafka_lag_monitor.main import run_remote_commands
 from kafka_lag_monitor.progress_bar import TuiProgressor
+from textual import work
 
 from kafka_lag_monitor.schemas import RemoteDetails
 from kafka_lag_monitor.utils import parse_and_agg_kafka_outputs
-
-# from textual.reactive import reactive
-
-# ROWS = [
-#     ("group", "topic", "partition", "lag"),
-#     (4, "Joseph Schooling", "Singapore", 50.39),
-#     (2, "Michael Phelps", "United States", 51.14),
-#     (5, "Chad le Clos", "South Africa", 51.14),
-#     (6, "László Cseh", "Hungary", 51.14),
-#     (3, "Li Zhuhao", "China", 51.26),
-#     (8, "Mehdy Metella", "France", 51.58),
-#     (7, "Tom Shields", "United States", 51.73),
-#     (1, "Aleksandr Sadovnikov", "Russia", 51.84),
-#     (10, "Darren Burns", "Scotland", 51.84),
-# ]
-
 
 class TestApp(App):
     """A textual app to manage stopwatches"""
@@ -32,21 +17,9 @@ class TestApp(App):
     progressor: TuiProgressor
     table: DataTable
 
-    # CSS_PATH = "stopwatch03.tcss"
-    BINDINGS = [("d", "toggle_dark", "Toggle dark mode"), ("r", "refresh", "Refresh Data")]
-
-    # def on_button_pressed(self, event: Button.Pressed) -> None:
-    #     """Event handler when button pressed"""
-    #     button_id = event.button.id
-    #     time_display = self.query_one(TimeDisplay)
-    #     if button_id == "start":
-    #         time_display.start()
-    #         self.add_class("started")
-    #     elif button_id == "stop":
-    #         time_display.stop()
-    #         self.remove_class("started")
-    #     elif button_id == "reset":
-    #         time_display.reset()
+    BINDINGS = [
+        ("d", "toggle_dark", "Toggle dark mode"),
+    ]
 
     def compose(self) -> ComposeResult:
         """Create child widgets for app"""
@@ -55,9 +28,6 @@ class TestApp(App):
         self.table.add_columns("group", "topic", "partition", "lag_mean", "lag_max")
         yield Header()
         yield Footer()
-        # TODO: Read about textual workers, have to run progress bar and refresh concurrently to make it work properly
-        # https://github.com/Textualize/textual/discussions/3135
-        # https://textual.textualize.io/guide/workers/
         yield ScrollableContainer(self.progressor.progress_bar, self.table, id="layout")
 
     def action_toggle_dark(self) -> None:
@@ -67,13 +37,17 @@ class TestApp(App):
         self._refresh_data()
 
     def on_mount(self) -> None:
-        pass
-        # self._refresh_data()
+        self._refresh_data()    #Initial refresh
+        self.set_interval(15, self._refresh_data)    #Set interval for every 5s (TODO: Maybe configurable?)
+
+    @work(exclusive=True, thread=True)
     def _refresh_data(self):
+        self.progressor.progress_bar.update(progress=0)
         command_outputs = run_remote_commands(
             self.remote_details, self.commands, False, self.progressor
         )
         df = parse_and_agg_kafka_outputs(command_outputs)
+        self.table.clear()
         for _, row in df.iterrows():
             tupled_row = (
                 row["group"],
@@ -85,7 +59,6 @@ class TestApp(App):
             self.table.add_row(
                 *tupled_row, key=f"{row['group']}-{row['topic']}"
             )  # TODO: better way to convert
-        # table.add_rows(df.itertuples(index=False))
 
 
 if __name__ == "__main__":
